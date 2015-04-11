71288e9ec1a30f2d6ae724a524af519d7557c3b6
emdimacali <ejandramae.16@gmail.com>
2015-04-11 17:22:35 +0800

    Making repo for ragsdale project

---
+++ sample/application/views/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> Ragsdale </title>
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-theme.css">
+		<!--link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-theme.min.css"-->
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/home.css">
+		<script type="javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div id="header">
+			<a href="<?php echo base_url();?>ragsdale/tanga" class="button1"> Home |</a>
+			<a type="button" class="button1" >Services |</a>
+			<a type="button" class="button1"> Products </a>
+			<a href="<?php echo base_url();?>application/views/home.php" id="placeholder">  </a>
+			<a type="button" class="button2"> Blog |</a>
+			<a type="button" class="button2"> About Us |</a>
+			<a type="button" class="button2"> Contact Us </a>
+		</div>
+		<div id = "announcements">
+		THIS IS THE ANNOUNCEMENTS
+		</div>
+		<div id = "textBody">
+		THIS IS WHERE YOU PARA THE GRAPH.
+		</div>
+		<div id = "contacts">
+		THIS IS WHERE CONTACTS
+		</div>
+		<div id = "getInTouch">
+		THIS IS WHERE YOU TOUCH
+		</div>
+		<div id = "recentPosts">
+		THIS IS WHERE YOU RECENT THE POST
+		</div>
+		<div id = "footer">
+		THIS IS WHERE YOU FOOT
+		</div>
+		<!--<?php echo "Hello World!"?>-->
+	<body>
+</html>
